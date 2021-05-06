d66b993499d66a1bfc516b75741ff3516214d6d2
Bartłomiej Umiński <bartlomiejuminski1999@gmail.com>
2021-05-06 18:15:48 +0200

    poruszanie czołgiem

--- game.js
+++ game.js
@@ -33,58 +33,23 @@ function create() {
     tank.x = 400;
     tank.y = 300;
     tank.body.setCollideWorldBounds(true);
-
-    tank.body.velocity.x = 100;
-    tank.body.velocity.y = -150;
 }
 
 function update() {
-    /*tank.body.velocity.x = 0;
-    tank.body.velocity.y = 0;*/
+
+    // Tank - movement
+    tank.body.velocity.x = 0;
+    tank.body.velocity.y = 0;
     if (cursors.left.isDown) {
         tank.body.velocity.x = -250;
     }
-    else if (cursors.right.isDown) {
-        tank.body.velocity.x = 250;
-    }
-    if (cursors.up.isDown) {
-        tank.body.velocity.y = -250;
-    }
-    else if (cursors.down.isDown) {
-        tank.body.velocity.y = 250;
-    }
-
-    /*if (cursors.left.isDown) {
-        tank.body.velocity.x = -250;
-    } else {
-        tank.body.velocity.x = 0;
-    }
     if (cursors.right.isDown) {
         tank.body.velocity.x = 250;
-    } else {
-        tank.body.velocity.x = 0;
     }
     if (cursors.up.isDown) {
         tank.body.velocity.y = -250;
-    } else {
-        tank.body.velocity.y = 0;
     }
     if (cursors.down.isDown) {
         tank.body.velocity.y = 250;
-    } else {
-        tank.body.velocity.y = 0;
-    }*/
-
-    if (cursors.left.isUp) {
-        tank.body.velocity.x = 0;
-    }
-    if (cursors.right.isUp) {
-        tank.body.velocity.x = 0;
-    }
-    if (cursors.up.isUp) {
-        tank.body.velocity.y = 0;
-    }
-    if (cursors.down.isUp) {
-        tank.body.velocity.y = 0;
     }
 }
